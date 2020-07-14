a2c44750e50a65db99954ce1ce66ff004502d780
lumengxin <2735146963@qq.com>
2020-07-14 10:46:37 +0800

    简单的express展示ts爬取的数据

---
+++ src/analyzer/baiduAnalyzer.ts
@@ -0,0 +1,64 @@
+import cheerio from 'cheerio'
+import fs from 'fs'
+import { Analyzer } from '../crowller'
+
+interface Course {
+  rank: number
+  title: string
+}
+interface CourseResult {
+  time: number
+  data: Course[]
+}
+interface Content {
+  [propName: number]: Course[]
+}
+
+export default class BaiduAnalyzer implements Analyzer {
+  private static instance: BaiduAnalyzer
+
+  // 单例模式
+  static getInstance() {
+    if (!BaiduAnalyzer.instance) {
+      BaiduAnalyzer.instance = new BaiduAnalyzer()
+    }
+    return BaiduAnalyzer.instance
+  }
+
+  private getCourseInfo(html: string) {
+    const $ = cheerio.load(html)
+    const bookBox = $('.hotsearch-item')
+
+    const courseInfo: Course[] = []
+
+    bookBox.map((index, element) => {
+      const descs = $(element).find('a > span')
+      const rank = parseInt(descs.eq(0).text(), 10)
+      const title = descs.eq(1).text()
+      console.log("BaiduAnalyzer -> getCourseInfo -> title",rank, title)
+      courseInfo.push({rank, title})
+    })
+  
+    return {
+      time: new Date().getTime(),
+      data: courseInfo
+    }
+  }
+
+  private generateJsonContent(courseInfo: CourseResult, filePath: string) {
+    let fileContent: Content = {}
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+    }
+    fileContent[courseInfo.time] = courseInfo.data
+    return fileContent
+  }
+
+  public analyzer(html: string, filePath: string) {
+    const courseInfo = this.getCourseInfo(html)
+    const fileContent = this.generateJsonContent(courseInfo, filePath)
+    return JSON.stringify(fileContent)
+  }
+
+  private constructor() {}
+}
---
+++ src/crowller.ts
@@ -0,0 +1,41 @@
+// ts 引用 js， 通过.d.ts翻译 npm install @types/superagent -D
+import superagent from 'superagent'
+import fs from 'fs'
+import path from 'path'
+import BaiduAnalytics from './analyzer/baiduAnalyzer'
+
+export interface Analyzer {
+  analyzer: (html: string, filePath: string) => string
+}
+
+class Crowller {
+  private filePath = path.resolve(__dirname, '../data/course.json')
+
+  private async getRawHtml() {
+    const result = await superagent.get(this.url)
+    return result.text
+  }
+
+  private writeFile(content: string) {
+    fs.writeFileSync(this.filePath, content)
+  }
+
+  private async initSpiderProcess() {
+    const html = await this.getRawHtml()
+    const fileContent = this.baiduAnalyzer.analyzer(html, this.filePath)
+    this.writeFile(fileContent)
+  }
+
+  constructor(private url: string, private baiduAnalyzer: Analyzer) {
+    this.initSpiderProcess()
+  }
+}
+
+export default Crowller
+
+// const url = `http://www.baidu.com/`
+
+// // const baiduAnalyzer = new BaiduAnalytics()
+// const baiduAnalyzer = BaiduAnalytics.getInstance()
+
+// new Crowller(url, baiduAnalyzer)
